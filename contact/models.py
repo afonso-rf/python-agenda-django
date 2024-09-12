78dbcfb77a84ec0eb39f19317f7b0738b32a8059
Afonso Filho <afonso.rf@gmail.com>
2024-09-12 07:34:56 -0300

    :sparkles: feat: Criado modulo

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# id (primary key ) - automatic
+# first_name (string), last_name (string), phone (string),
+# email (email), description (text), created_date (date), updated_date (date),
+# category (foreign key), show (boolean), owner (foreign key)
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250, blank=True)
+    # category = ...
+    description = models.TextField(blank=True)
+    # show = ...
+    # owner = ...
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
