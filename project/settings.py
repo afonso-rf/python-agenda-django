8c1a720228dbfb27d46957ccf58f6adaa19ce15b
Afonso Filho <afonso.rf@gmail.com>
2024-09-16 06:44:04 -0300

    :constructions: Alterado settings para usar vareaveis locais

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "base_static",
 ]
 
-STATIC_ROOT = BASE_DIR  / "static"  # collectstatic
+STATIC_ROOT = BASE_DIR / "static"  # collectstatic
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -132,3 +132,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
