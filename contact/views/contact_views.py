b3e7e01c6337ec3315759d7ae6d2ba5827009351
Afonso Filho <afonso.rf@gmail.com>
2024-09-20 18:31:26 -0300

    :sparkles: feat: Adicionado pesquisa e paginacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.db.models import Q
 from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by("-id")
-
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
         "site_title": "Contatos - ",
-        "contacts": contacts,
+        "page_obj": page_obj,
     }
 
     return render(
@@ -26,7 +30,7 @@ def contact(request, contact_id):
         show=True,
     )
     site_title = f"{sigle_contact.first_name} {sigle_contact.last_name} - "
-    
+
     context = {
         "site_title": site_title,
         "contact": sigle_contact,
@@ -35,5 +39,35 @@ def contact(request, contact_id):
     return render(
         request=request,
         template_name="contact/contact.html",
+        context=context,
+    )
+
+
+def search(request):
+    name_search = request.GET.get("q", "").strip()
+    contacts_search = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=name_search)
+        | Q(last_name__icontains=name_search)
+        | Q(phone__icontains=name_search)
+        | Q(email__icontains=name_search),
+    )
+    if name_search == "":
+        return redirect("contact:index")
+
+    input_value = name_search
+
+    paginator = Paginator(contacts_search, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "site_title": "Search - ",
+        "page_obj": page_obj,
+        "input_value": input_value,
+    }
+
+    return render(
+        request,
+        template_name="contact/index.html",
         context=context,
     )
